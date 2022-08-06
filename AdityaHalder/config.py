f1b82f32ee486b5ef71e7bc1ea23643ed579b1b3
Oyehoye99922 <103871412+Oyehoye99922@users.noreply.github.com>
2022-08-06 11:52:33 +0530

    Update config.py

--- AdityaHalder/config.py
+++ AdityaHalder/config.py
@@ -1,23 +1,41 @@
 import os
+
 import aiohttp
+
 from os import getenv
+
 from dotenv import load_dotenv
+
     
+
 if os.path.exists("Internal"):
+
     load_dotenv("Internal")
 
 aiohttpsession = aiohttp.ClientSession()
+
 admins = {}
+
 que = {}
 
 API_ID = int(getenv("API_ID", ""))
+
 API_HASH = getenv("API_HASH", "")
+
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
+
+STRING_SESSION = getenv("STRING_SESSION", "session")
+
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". ! /").split())
+
 MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/GeniusBoi/Genius-Userbot")
+
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Oyehoye99922/userbotRJ")
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Romeo_RJ")
